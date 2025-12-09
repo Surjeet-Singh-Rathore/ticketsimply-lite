3f8b84c01a599e3dde6f7341a758496306a1b02f
MohitShrzy <mohits@bitlasoft.com>
2025-12-09 13:37:58 +0530

    Removed current location access popup

--- app/src/main/java/com/bitla/ts/presentation/view/dashboard/DashboardNavigateActivity.kt
+++ app/src/main/java/com/bitla/ts/presentation/view/dashboard/DashboardNavigateActivity.kt
@@ -643,34 +643,7 @@ class DashboardNavigateActivity : BaseActivity(),
             }
         }
     }
-
-
-    private fun requestForLocationPermission() {
-        ActivityCompat.requestPermissions(
-            this,
-            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-            LOCATION_PERMISSION_REQUEST_CODE
-        )
-    }
-
-    @RequiresApi(Build.VERSION_CODES.M)
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
-            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                // Permission is granted. Continue the action or workflow in your app.
-                getLastKnownLocation()
-            } else {
-                toast("Location Permission Denied")
-
-                // Permission denied. Inform the user that the feature requires permission.
-            }
-        }
-    }
+    
 
     @RequiresApi(Build.VERSION_CODES.M)
     private fun getLastKnownLocation() {
@@ -1108,13 +1081,6 @@ class DashboardNavigateActivity : BaseActivity(),
 
                             currentCountry = privilegeResponse?.country ?: ""
 
-                            if (!currentCountry.isNullOrEmpty() && currentCountry.equals(
-                                    "india",
-                                    true
-                                )
-                            ) {
-                                requestForLocationPermission()
-                            }
 
 
                             if (role == getString(R.string.role_agent) && privilegeResponse?.allowBookingForAllotedServices == true) {
